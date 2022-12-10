47b516a4d220e96f31ff934312ca837b2a7350a5
Anastasia Sys <stasy.sys@mail.ru>
2022-12-11 00:47:41 +0100

    feet: Added the funtionality to remove items from history. This is the first version with all the requirements.

--- index.js
+++ index.js
@@ -10,39 +10,57 @@ submitTransaction.addEventListener("submit", (e) => {
         let newTransaction = {concept: conceptInputElement.value, quantity: quantityInputElement.value};
         transactions.push(newTransaction);
         localStorage.setItem('transactions', JSON.stringify(transactions));
-        addTransaction(newTransaction);
+        addElements(newTransaction);
         
         conceptInputElement.value = "";
         quantityInputElement.value = "";
     }
 })
 
-function addTransaction(newTransaction){
+function addElements(newTransaction){
     const EXPENSE_COLOR = 'rgb(200, 60, 60)'
     const newTransactionList = document.createElement('li');
     const incomeElement = document.getElementById('income-p');
     const expenseElement = document.getElementById('expense-p');
     const savingsElement = document.getElementById('savings-p');
     if (parseInt(newTransaction.quantity) < 0) {
-        expenseElement.innerHTML =`${parseFloat(expenseElement.innerHTML) + parseFloat(newTransaction.quantity)}`;
+        expenseElement.innerHTML =`${(parseFloat(expenseElement.innerHTML) + parseFloat(newTransaction.quantity)).toFixed(2)}`;
         newTransactionList.style.background = EXPENSE_COLOR
     } else {
-        incomeElement.innerHTML =`${parseFloat(incomeElement.innerHTML) + parseFloat(newTransaction.quantity)}`;
-
+        incomeElement.innerHTML =`${(parseFloat(incomeElement.innerHTML) + parseFloat(newTransaction.quantity)).toFixed(2)}`;
     }
-savingsElement.innerHTML =`${parseFloat(incomeElement.innerHTML) + parseFloat(expenseElement.innerHTML)}`;
+
+    savingsElement.innerHTML =`${(parseFloat(incomeElement.innerHTML) + parseFloat(expenseElement.innerHTML)).toFixed(2)}`;
     const ulElement = document.getElementById('history-ul');
     newTransactionList.innerHTML = `
+        <button class="remove" onclick="removeTransaction(this)">❌</button>
         <div>
-            <p id="concept-li-p">${newTransaction.concept}</p>
-            <p id="quantity-li-p">${newTransaction.quantity}</p>
+            <div class="transaction">
+                <p id="concept-li-p">${newTransaction.concept}</p>
+                <p id="quantity-li-p">${newTransaction.quantity}</p>
+            </div>
         </div>
     `;
     ulElement.appendChild(newTransactionList);
 }
 
+function removeTransaction(button) {
+    const listElement = button.parentNode;
+    const ulElement = listElement.parentNode;
+    listElement.setAttribute('id', 'remove-li');
+    const transactionIndex = Array.from(ulElement.children, (x)  => {return x.id}).indexOf("remove-li");
+    transactions = localStorage.getItem('transactions');
+    transactions = data ? JSON.parse(data) : [];
+    transactions.splice(transactionIndex, 1);
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+    console.log(localStorage.getItem("transactions"))
+    listElement.remove();
+    location.reload();
+    
+}
+
 transactions = localStorage.getItem('transactions');
 transactions = data ? JSON.parse(data) : [];
 transactions.forEach((element) => {
-    addTransaction(element);
+    addElements(element);
 })
